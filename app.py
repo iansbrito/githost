8368be5c15f7c6fc17acb93faf2b38aa9eec4fc7
iansbrito <118158150+iansbrito@users.noreply.github.com>
2023-01-25 09:50:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,44 @@ def calculator():
     c = (a**2 + b**2)**0.5
     
     return jsonify({'a': a,'b':b, 'c':c})
+@app.route('/ca', methods=['POST'])
+def ca():
+    dados=json.loads(request.data)
+    a= str(dados.get('a'))
+    b= str(dados.get('b'))
+    if not a or not b:
+        return make_response("Informe o número de A e B.", 400)
+    try:    
+        a=float(a)
+        b=float(b)
+    except ValueError:
+        return make_response("Informe o número de A e B.", 400)
+    if a>b:
+        co = float(b)
+        h = float(a)
+    if b>a:
+        co=float(a)
+        h=float(b)
+    ca=((h**2)-(co**2))**(1/2)
+    
+    return jsonify({'a': a,'b':b, 'c':ca})
+@app.route('/co', methods=['POST'])
+def co():
+    dados=json.loads(request.data)
+    a= str(dados.get('a'))
+    b= str(dados.get('b'))
+    if not a or not b:
+        return make_response("Informe o número de A e B.", 400)
+    try:    
+        a=float(a)
+        b=float(b)
+    except ValueError:
+        return make_response("Informe o número de A e B.", 400)
+    ca = float(a)
+    h = float(b)
+    co=((h**2)-(ca**2))**(1/2)
+    
+    return jsonify({'a': a,'b':b, 'c':co})
 
 if __name__ == '__main__':
     app.run(debug=True)
