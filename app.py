782d170209b9de3db7c7c7d98dc1b138d58f8f47
iansbrito <118158150+iansbrito@users.noreply.github.com>
2023-01-25 04:39:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+import json
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/")
+CORS(app)
+@app.route('/calculator', methods=['POST'])
+def calculator():
+    dados=json.loads(request.data)
+    a= str(dados.get('a'))
+    b= str(dados.get('b'))
+    if not a or not b:
+        return make_response("Informe o número de A e B.", 400)
+    try:    
+        a=float(a)
+        b=float(b)
+    except ValueError:
+        return make_response("Informe o número de A e B.", 400)
+    c = (a**2 + b**2)**0.5
+    
+    return jsonify({'a': a,'b':b, 'c':c})
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True)
